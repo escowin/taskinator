01b97bf6db1f9b9739d9abdef428ea4918fd8e07
escowin <edwin@escowinart.com>
2022-04-12 17:56:55 -0500

    4.1.8 | js:1-11, code refactor. ability to add task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 var buttonEl = document.querySelector('#save-task');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-buttonEl.addEventListener('click', function() { // user click:
-  var listItemEl = document.createElement('li'); // create new task item
-  listItemEl.className = 'task-item'; // style new task item
-  listItemEl.textContent = 'new task'; // add text
-  tasksToDoEl.appendChild(listItemEl); // append element to task list
-});
+var createTaskHandler = function() { // this callback function *must* be placed before event listener
+  var listItemEl = document.createElement('li'); // new <li></li>
+  listItemEl.className = 'task-item'; // <li class='task-item'></li>
+  listItemEl.textContent = 'new task.'; // <li class='task-item'> new task.</li>
+  tasksToDoEl.appendChild(listItemEl); // append new <li></li> to task list <ul></ul>
+}
+
+buttonEl.addEventListener('click', createTaskHandler); // event listener *must* be placed after callback function
