94265bc1439ee2a83f9b168725188ad96ae1f882
escowin <edwin@escowinart.com>
2022-04-13 00:33:57 -0500

    4.2.6 | js:4-20 html via js, nested appending

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function() { // this callback function *must* be placed before event listener
+var createTaskHandler = function(event) { // this callback function *must* be placed before event listener
   event.preventDefault(); // stops browser's default refresh page behavior
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-  var listItemEl = document.createElement('li'); // new <li></li>
-  listItemEl.className = 'task-item'; // <li class='task-item'></li>
-  listItemEl.textContent = 'new task.'; // <li class='task-item'> new task.</li>
-  tasksToDoEl.appendChild(listItemEl); // append new <li></li> to task list that's within <ul></ul>
+  var listItemEl = document.createElement('li'); // create <li>
+  listItemEl.className = 'task-item'; // <li class='task-item'>
+
+  var taskInfoEl = document.createElement("div"); // create <div>
+  taskInfoEl.className = "task-info"; // <div class='task-info'>
+
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>"; // <div><h3>...</><span>...</></div>
+  
+  listItemEl.appendChild(taskInfoEl); // <li><div>...</></li>
+  tasksToDoEl.appendChild(listItemEl); // <ul><li>...</></ul>
+  console.dir(listItemEl);
 }
 
 // 'submit' reacts to <button type='submit> & hitting enter via keyboard
