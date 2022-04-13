c5c24a0c31c0f875d1978b880e08c4b0b3e2a393
escowin <edwin@escowinart.com>
2022-04-13 16:05:09 -0500

    4.3.6 | js:1, 42-60 html in memory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0;
 var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
@@ -22,16 +23,41 @@ var taskFormHandler = function(event) { // this callback function *must* be plac
 }
 
 var createTaskEl = function(taskDataObj) {
-  var listItemEl = document.createElement('li'); // create <li>
+  var listItemEl = document.createElement('li'); // <li>
   listItemEl.className = 'task-item'; // <li class='task-item'>
 
-  var taskInfoEl = document.createElement("div"); // create <div>
+  listItemEl.setAttribute("data-task-id", taskIdCounter); // <li class='task-item' data-task-id='taskIdCounter(0)'>
+
+  var taskInfoEl = document.createElement("div"); // <div>
   taskInfoEl.className = "task-info"; // <div class='task-info'>
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>"; // <div><h3>...</><span>...</></div>
-
   listItemEl.appendChild(taskInfoEl); // <li><div>...</></li>
-  tasksToDoEl.appendChild(listItemEl); // <ul><li>...</></ul>
-}
 
+  tasksToDoEl.appendChild(listItemEl); // <ul><li>...</></ul>
+
+  // increase task counter for next unique id
+  taskIdCounter++;
+};
+
+var createTaskActions = function(taskId) {
+  var actionContainerEl = document.createElement("div"); // <div>
+  actionContainerEl.className = "task-actions"; // <div class='task-actions'>
+
+  var editButtonEl = document.createElement("button"); // <button>
+  editButtonEl.textContent = "edit"; // <button>edit</>
+  editButtonEl.className = "btn edit-btn"; // <button class="btn edit-btn">edit</>
+  editButtonEl.setAttribute("data-task-id", taskId); // <button class="btn edit-btn" data-task-id="taskId()">edit</>
+  
+  actionContainerEl.appendChild(editButtonEl); // <div...><button...>edit</></>
+
+  // delete button
+  var deleteButtonEl = document.createElement("button");
+  deleteButtonEl.textContent = "delete";
+  deleteButtonEl.className = "btn delete-btn";
+  deleteButtonEl.setAttribute("data-task-id", taskId);
+
+  actionContainerEl.appendChild(deleteButtonEl);
+  return actionContainerEl;
+};
 // 'submit' reacts to <button type='submit> & hitting enter via keyboard
 formEl.addEventListener('submit', taskFormHandler); // event listener *must* be placed after callback function
