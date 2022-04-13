355281523ff1d3968f216889a7c0ef7acb50308a
escowin <edwin@escowinart.com>
2022-04-12 23:49:24 -0500

    4.2.5 | js:5 event, var buttonEl to var formEl

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,14 @@
-var buttonEl = document.querySelector('#save-task');
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
 var createTaskHandler = function() { // this callback function *must* be placed before event listener
+  event.preventDefault(); // stops browser's default refresh page behavior
+
   var listItemEl = document.createElement('li'); // new <li></li>
   listItemEl.className = 'task-item'; // <li class='task-item'></li>
   listItemEl.textContent = 'new task.'; // <li class='task-item'> new task.</li>
-  tasksToDoEl.appendChild(listItemEl); // append new <li></li> to task list <ul></ul>
+  tasksToDoEl.appendChild(listItemEl); // append new <li></li> to task list that's within <ul></ul>
 }
 
-buttonEl.addEventListener('click', createTaskHandler); // event listener *must* be placed after callback function
+// 'submit' reacts to <button type='submit> & hitting enter via keyboard
+formEl.addEventListener('submit', createTaskHandler); // event listener *must* be placed after callback function
