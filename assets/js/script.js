d93bff3ee3bb3685e2a3cb33b6caf09f85b27ffc
escowin <edwin@escowinart.com>
2022-07-23 13:57:21 -0500

    4.2.6 | functional createTaskHandler

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,32 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// logic  | + <li class="task-item">new task</li>; <ul><li.../></ul>
+// logic  | + tasks to document
 var createTaskHandler = function(event) {
-  // prevents browser refresh
   event.preventDefault();
 
+  // select <element [attribute]>, get value
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+  // + <li class"task-item"></li>
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "new task";
+
+  // + <div class="task-info"><h3>...</h3></div>
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info"
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  // append <ul><li><div... /></li></ul>
+  listItemEl.appendChild(taskInfoEl);
   tasksToDoEl.appendChild(listItemEl);
+
+  console.dir(listItemEl);
 }
 
 // event listener | submit <#task-form>, invoke createTaskHandler
 // submit listens for either:
-//    1. mouse clicks on <... type="submit">
+//    1. clicks on <... type="submit">
 //    2. pressing enter on keyboard
 formEl.addEventListener("submit", createTaskHandler);
