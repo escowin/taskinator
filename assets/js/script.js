a8fbe9a0635316e67058177883d443fb71b4bf68
escowin <edwin@escowinart.com>
2022-07-23 13:09:15 -0500

    js | + submit event listener, event.preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 // dom elements
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // logic  | + <li class="task-item">new task</li>; <ul><li.../></ul>
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+  // prevents browser refresh
+  event.preventDefault();
+
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "new task";
   tasksToDoEl.appendChild(listItemEl);
 }
 
-// event listener | click <#save-task>, invoke createTaskHandler
-buttonEl.addEventListener("click", createTaskHandler);
+// event listener | submit <#task-form>, invoke createTaskHandler
+// submit listens for either:
+//    1. mouse clicks on <... type="submit">
+//    2. pressing enter on keyboard
+formEl.addEventListener("submit", createTaskHandler);
